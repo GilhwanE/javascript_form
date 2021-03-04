42fe496f23492d4b2220105f1a53ba532e054e2a
GilhwanE <dyghks123@gmail.com>
2021-03-04 19:35:58 +0900

    JS : func(toggle)

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const dropdown = document.querySelector('.dropdown');
+const dropToggle = document.querySelector('.dropdown-toggle');
+const dropMenu = document.querySelector('.dropdown-menu');
+
+
+dropToggle.addEventListener("click" , () => {
+    dropMenu.classList.toggle('show');
+});
