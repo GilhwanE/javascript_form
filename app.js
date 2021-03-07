08e952cc913d1af33aaa98b39c6ff8568f867441
GilhwanE <dyghks123@gmail.com>
2021-03-07 19:34:09 +0900

    JS : func(item click)

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 'use strict';
 
-const dropdown = document.querySelector('.dropdown');
+const dropDown = document.querySelector('.dropdown');
 const dropToggle = document.querySelector('.dropdown-toggle');
 const dropMenu = document.querySelector('.dropdown-menu');
-
+const dropItme = document.querySelector('.dropdown-item');
+const dropOptions = document.querySelectorAll('.dropdown-option');
 
 dropToggle.addEventListener("click" , () => {
     dropMenu.classList.toggle('show');
@@ -13,3 +14,14 @@ dropToggle.addEventListener("blur", ()=> {
     dropMenu.classList.remove('show');
 });
 
+
+
+// option(item)에서 선택된값을 dropdown-toggle이름 표시란에 표시해야함
+dropOptions.forEach(function(item) {
+    item.addEventListener('click', (e) => {
+    const value = e.currentTarget.textContent.trim();
+    dropToggle.textContent = value;
+    dropToggle.classList.add('selected');
+
+})
+});
